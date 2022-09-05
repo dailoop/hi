6f8eb29efa8a2bebab160ab26e623e63aac55c5d
suyou <suyoutain41@gmail.com>
2022-09-05 22:31:32 +0800

    review Day1

---
+++ review.py
@@ -0,0 +1,6 @@
+x = 4                          #整數(宣告)
+xx = 5.4                        #浮點數
+xxxx = False                     #布林
+xxxxx = "你好"                    #字串
+y = input("are you crazy?: ")     #輸入
+print("idk")                       #打印
